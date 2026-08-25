bfc3d2dda0d3b9bbf19478186c62478c3e7f1c5c
Yaroslav Shevchuk <yarolegovich@gmail.com>
2026-08-25 10:00:30 +0000

    reuse destroy utility

--- internal/cli/task_cancel.go
+++ internal/cli/task_cancel.go
@@ -40,11 +40,7 @@ func newTaskCancelCmd(cfg *globalConfig) *cobra.Command {
 			if err != nil {
 				return fmt.Errorf("failed to create a client: %w", err)
 			}
-			defer func() {
-				if err := client.Destroy(); err != nil {
-					cfg.logf("failed to destroy client: %v", err)
-				}
-			}()
+			defer destroyClient(cfg, client)
 
 			req := &a2a.CancelTaskRequest{
 				ID:     a2a.TaskID(args[0]),
